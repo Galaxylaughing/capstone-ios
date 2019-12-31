d17fd297f1961e6530f33b26922cf00138db4f36
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-12-31 07:43:58 -0800

    Initial Commit

---
+++ LibAwesome/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  LibAwesome
+//
+//  Created by Sabrina on 12/31/19.
+//  Copyright © 2019 SabrinaLowney. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
