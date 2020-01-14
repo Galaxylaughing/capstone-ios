4ec4b3b56060dc13a25926ee227a36627200617a
Sabrina Lowney <sabrina@galaxylaughing.com>
2020-01-13 16:11:42 -0800

    updated book detail view

--- LibAwesome/BookDetailView.swift
+++ LibAwesome/BookDetailView.swift
@@ -10,64 +10,315 @@ import SwiftUI
 
 struct BookDetailView: View {
     @EnvironmentObject var env: Env
-//    @EnvironmentObject var book: BookList.Book
     static var book: BookList.Book = BookList.Book()
     
-    var body: some View {
-        VStack {
-            VStack { // title and author
-                ForEach(BookDetailView.book.findComponents(), id: \.self) { component in
-                    VStack {
-                        if component == BookDetailView.book.getMainTitle() {
-                            Text(BookDetailView.book.getMainTitle())
-                                .font(.largeTitle)
-                                .multilineTextAlignment(.center)
-                        } else {
-                            Text(component)
-                                .multilineTextAlignment(.center)
+    struct DisplayHeader: View {
+        fileprivate func displayHeader() -> some View {
+            return Section() {
+                VStack {
+                    ForEach(BookDetailView.book.findComponents(), id: \.self) { component in
+                        VStack {
+                            if component == BookDetailView.book.getMainTitle() {
+                                Text(BookDetailView.book.getMainTitle())
+                                    .font(.largeTitle)
+                                    .multilineTextAlignment(.center)
+                                    .padding(.horizontal)
+                            } else {
+                                Text(component)
+                                    .multilineTextAlignment(.center)
+                                    .padding(.horizontal)
+                            }
                         }
                     }
-                }
-                Text(BookDetailView.book.authorNames())
+                    Text(BookDetailView.book.authorNames())
                     .font(.headline)
                     .multilineTextAlignment(.center)
-                
-                VStack(alignment: .leading) {
-                    Section() { // series, position in series
-                        if BookDetailView.book.seriesId != nil {
-                            Text("# \(String(BookDetailView.book.position)) | \(self.getSeriesName())")
-                        }
-                    }
-                    .padding([.top, .leading])
-                    
-                    Section() { // tags
-                        HStack {
-                            VStack(alignment: .leading) {
-                                ForEach(Alphabetical(BookDetailView.book.tags), id: \.self) { tag in
-                                    TagBubble(text: tag)
-                                        .padding(.vertical, 4)
-                                }
-                            }
-                            .padding(.leading)
-                            Spacer()
-                        }
-                    }
                     .padding(.top)
                 }
+                .padding()
             }
-            Spacer()
         }
-//        .navigationBarTitle(Text(BookDetailView.book.getMainTitle()), displayMode: .inline)
-//        .navigationBarItems(trailing: EditBookButton().environmentObject(BookDetailView.book))
+        
+        var body: some View {
+            self.displayHeader()
+        }
     }
     
-    func getSeriesName() -> String {
-        if let series = self.env.seriesList.series.first(where: {$0.id == BookDetailView.book.seriesId}) {
-            return series.name
+    struct DisplaySeriesInfo: View {
+        @EnvironmentObject var env: Env
+        
+        fileprivate func getSeriesName() -> String {
+            if let series = self.env.seriesList.series.first(where: {$0.id == BookDetailView.book.seriesId}) {
+                return series.name
+            }
+            return "[unknown]"
         }
-        return "[unknown]"
+        
+        fileprivate func displaySeriesInfo() -> AnyView {
+            if BookDetailView.book.seriesId != nil {
+                return AnyView(
+                    VStack {
+                        Divider()
+                        Section() {
+                            HStack {
+                                Text("# \(String(BookDetailView.book.position))")
+                                Text("|")
+                                Text("\(self.getSeriesName())")
+                            }
+                        }
+                        .padding(.vertical)
+                    }
+                )
+            } else {
+                return AnyView(EmptyView())
+            }
+        }
+        
+        var body: some View {
+            self.displaySeriesInfo()
+        }
     }
     
+    struct DisplayTags: View {
+        @State private var showTags: Bool = false
+        
+        fileprivate func displayTags() -> AnyView {
+            return AnyView(
+                Section() {
+                    VStack {
+                        Button(action: { self.showTags.toggle() }) {
+                            HStack {
+                                Text("Tags")
+                                Spacer()
+                                if self.showTags {
+                                    Image(systemName: "chevron.up")
+                                } else {
+                                    Image(systemName: "chevron.down")
+                                }
+                            }
+                        }
+                        if self.showTags {
+                            if BookDetailView.book.tags.count > 0 {
+                                HStack {
+                                    Spacer()
+                                    VStack(alignment: .leading) {
+                                        ForEach(Alphabetical(BookDetailView.book.tags), id: \.self) { tag in
+                                            TagBubble(text: tag)
+                                                .padding(.vertical, 4.0)
+                                        }
+                                    }
+                                    Spacer()
+                                }
+                                .padding(.top)
+                            } else {
+                                Text("This book has no tags.")
+                                .padding(.top)
+                            }
+                        }
+                    }
+                    .padding()
+                }
+            )
+        }
+        
+        var body: some View {
+            self.displayTags()
+        }
+    }
+    
+    struct DisplayISBNs: View {
+        @State private var showISBNs: Bool = false
+        
+        fileprivate func listISBNs() -> AnyView {
+            var view: AnyView
+            if BookDetailView.book.isbn10 != nil
+            && BookDetailView.book.isbn13 != nil {
+                view = AnyView(
+                    VStack(alignment: .leading) {
+                        Text("ISBN-10: \(BookDetailView.book.isbn10!)")
+                        Text("ISBN-13: \(BookDetailView.book.isbn13!)")
+                    }
+                    .padding(.top)
+                )
+            } else if BookDetailView.book.isbn10 != nil {
+                view = AnyView(
+                    VStack(alignment: .leading) {
+                        Text("ISBN-10: \(BookDetailView.book.isbn10!)")
+                    }
+                    .padding(.top)
+                )
+            } else if BookDetailView.book.isbn13 != nil {
+                view = AnyView(
+                    VStack(alignment: .leading) {
+                        Text("ISBN-10: \(BookDetailView.book.isbn13!)")
+                    }
+                    .padding(.top)
+                )
+            } else {
+                view = AnyView(EmptyView())
+            }
+            return view
+        }
+        
+        fileprivate func displayISBNs() -> AnyView {
+            return AnyView(
+                VStack {
+                    Button(action: { self.showISBNs.toggle() }) {
+                        HStack {
+                            Text("Identifiers")
+                            Spacer()
+                            if self.showISBNs {
+                                Image(systemName: "chevron.up")
+                            } else {
+                                Image(systemName: "chevron.down")
+                            }
+                        }
+                    }
+                    if self.showISBNs {
+                        if (BookDetailView.book.isbn10 != nil)
+                        || (BookDetailView.book.isbn13 != nil) {
+                            self.listISBNs()
+                        } else {
+                            Text("No ISBNs available.")
+                        }
+                    }
+                }
+                .padding()
+            )
+        }
+
+        var body: some View {
+            self.displayISBNs()
+        }
+    }
+    
+    struct DisplayPublicationInfo: View {
+        @State private var showPublicationInfo: Bool = false
+        
+        fileprivate func listPublicationInfo() -> AnyView {
+            return AnyView(
+                VStack {
+                    if BookDetailView.book.pageCount != nil {
+                        Text("\(BookDetailView.book.pageCount!) pages")
+                    }
+                    if BookDetailView.book.publicationDate != nil {
+                        Text("published \(BookDetailView.book.publicationDate!)")
+                    }
+                    if BookDetailView.book.publisher != nil {
+                        Text("by \(BookDetailView.book.publisher!)")
+                    }
+                }
+                .padding(.top)
+            )
+        }
+        
+        fileprivate func displayPublicationInfo() -> AnyView {
+            return AnyView(
+                VStack {
+                    Button(action: { self.showPublicationInfo.toggle() }) {
+                        HStack {
+                            Text("Publication Info")
+                            Spacer()
+                            if self.showPublicationInfo {
+                                Image(systemName: "chevron.up")
+                            } else {
+                                Image(systemName: "chevron.down")
+                            }
+                        }
+                    }
+                    if self.showPublicationInfo {
+                        if (BookDetailView.book.pageCount != nil)
+                        || (BookDetailView.book.publicationDate != nil)
+                        || (BookDetailView.book.publisher != nil) {
+                            self.listPublicationInfo()
+                        } else {
+                            Text("No publication information available.")
+                            .padding(.top)
+                        }
+                    }
+                }
+                .padding()
+            )
+        }
+
+        var body: some View {
+            self.displayPublicationInfo()
+        }
+    }
+    
+    struct DisplayDescription: View {
+        @State private var showDescription: Bool = false
+        
+        fileprivate func listDescription() -> some View {
+            var view: AnyView
+            if BookDetailView.book.description != nil {
+                view = AnyView(
+                    Text("\(BookDetailView.book.description!)")
+                )
+            } else {
+                view = AnyView(
+                    Text("No description available.")
+                )
+            }
+            return view
+        }
+        
+        fileprivate func displayDescription() -> AnyView {
+            return AnyView(
+                VStack {
+                    Button(action: { self.showDescription.toggle() }) {
+                        HStack {
+                            Text("Description")
+                            Spacer()
+                            if self.showDescription {
+                                Image(systemName: "chevron.up")
+                            } else {
+                                Image(systemName: "chevron.down")
+                            }
+                        }
+                    }
+                    if self.showDescription {
+                        self.listDescription()
+                        .padding()
+                    }
+                }
+                .padding()
+            )
+        }
+
+        var body: some View {
+            self.displayDescription()
+        }
+    }
+    
+    // BOOK DETAIL VIEW BODY
+    var body: some View {
+        ScrollView {
+            VStack {
+                VStack {
+                    Group {
+                        DisplayHeader()
+                    }
+                    
+                    Group {
+                        DisplaySeriesInfo()
+                        Divider()
+                        DisplayTags()
+                        Divider()
+                    }
+
+                    Group {
+                        DisplayISBNs()
+                        Divider()
+                        DisplayPublicationInfo()
+                        Divider()
+                        DisplayDescription()
+                    }
+                }
+                Spacer()
+            }
+        }
+    }
 }
 
 struct BookDetailView_Previews: PreviewProvider {
@@ -85,7 +336,16 @@ struct BookDetailView_Previews: PreviewProvider {
         ],
         position: 1,
         seriesId: 1,
-        tags: ["non-fiction", "fantasy", "science-fiction", "mystery", "fantasy/contemporary"])
+        publisher: "Harper Collins",
+        publicationDate: "2011-06-28",
+        isbn10: "0061991120",
+        isbn13: "9780061991127",
+        pageCount: 432,
+        description: """
+        The classic collaboration from the internationally bestselling authors Neil Gaiman and Terry Pratchett. According to The Nice and Accurate Prophecies of Agnes Nutter, Witch (the world's only completely accurate book of prophecies, written in 1655, before she exploded), the world will end on a Saturday. Next Saturday, in fact. Just before dinner. So the armies of Good and Evil are amassing, Atlantis is rising, frogs are falling, tempers are flaring. Everything appears to be going according to Divine Plan. Except a somewhat fussy angel and a fast-living demon—both of whom have lived amongst Earth's mortals since The Beginning and have grown rather fond of the lifestyle—are not actually looking forward to the coming Rapture. And someone seems to have misplaced the Antichrist.
+        """,
+        tags: ["fantasy", "science-fiction", "comedy", "fantasy/contemporary"]
+    )
     
     static var seriesList = SeriesList(series: [series1])
     static var bookList = BookList(books: [exampleBook])
@@ -101,6 +361,5 @@ struct BookDetailView_Previews: PreviewProvider {
     static var previews: some View {
         BookDetailView()
             .environmentObject(self.env)
-//            .environmentObject(self.exampleBook)
     }
 }
